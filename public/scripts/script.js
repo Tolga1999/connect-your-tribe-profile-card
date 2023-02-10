46ae7dbeabacd7be8d6189ad0632486ac17b68a3
Tolga1999 <tolgadoganer@outlook.com>
2023-02-10 15:22:55 +0100

    deels custom muis gefixt

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,10 @@
+// selecteren van elementen/classes
+let mouseContainer = document.querySelector('.custom-mouse-container');
+let body = document.querySelector('body');
 
+body.addEventListener('mousemove', function(coordinates){
+    mouseContainer.style.top = coordinates.clientY + "px";
+    mouseContainer.style.left = coordinates.clientX + "px";
+})
 
 
