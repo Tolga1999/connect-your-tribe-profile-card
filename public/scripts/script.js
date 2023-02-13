47f9772ce6aecf180d1993d827e12a74d5207e1a
Tolga1999 <tolgadoganer@outlook.com>
2023-02-13 03:59:06 +0100

    text animations and more circle containers

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,9 +2,18 @@
 let mouseContainer = document.querySelector('.custom-mouse-container');
 let body = document.querySelector('body');
 
-body.addEventListener('mousemove', function(coordinates){
+// selecteren van alle tekst elementen/classes voor text animatie
+let heading1Name = document.querySelector('h1');
+let paragraphAboutMe = document.querySelector('p');
+
+// eventlistener voor coordinaten van muis bij mousemove
+body.addEventListener('mousemove', function (coordinates) {
     mouseContainer.style.top = coordinates.clientY + "px";
     mouseContainer.style.left = coordinates.clientX + "px";
 })
 
-
+// page reload is gelijk aan het laden van je text in met JS
+window.onload = function () {
+    heading1Name.classList.toggle('showText');
+    paragraphAboutMe.classList.toggle('showText');
+}
